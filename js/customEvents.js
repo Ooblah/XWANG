371415250cca8c8b8c9927de3f3499ce3eb20b54
Xintong Wang <xwang@loyalty.com>
2014-05-16 15:41:56 -0400

    design change and some experimenting

---
+++ js/customEvents.js
@@ -0,0 +1,46 @@
+jQuery(function($) {
+	// panelSnap plugin initiation
+    $('body').panelSnap({
+    	$menu: $('.menu'),
+    	slideSpeed: 500,
+    	keyboardNavigation: {
+    		enable:true
+    	}
+    });
+
+    //vertical center
+    $('.menu').css({
+        'top': '50%',
+        'margin-top': -$('.menu').height()/2
+    });
+
+});
+
+
+//scroll effects
+$(window).scroll(function() {
+// fade in point of new nav at second page
+
+	if ($(window).scrollTop() > $(window).height()/1.2 ){
+		//vertical nav
+ 		$('.menu').stop().animate({
+   			opacity : 0.8
+   		}, 50 );
+
+   		//horizontal nav
+   		$('.menu-h').stop().animate({
+   			opacity : 0
+   		}, 50 );
+
+  	} else {
+  		//vertical nav
+		$('.menu').stop().animate({
+   			opacity : 0
+   		}, 30 );
+
+   		//horizontal nav
+   		$('.menu-h').stop().animate({
+   			opacity : 1
+   		}, 50 );	
+ 	};   	
+});
