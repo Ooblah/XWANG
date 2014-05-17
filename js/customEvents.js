2a115df9c386006722e70a15dc58a5da43c957f5
Ooblah <thewonderbird@hotmail.com>
2014-05-17 17:13:48 -0400

    image added

--- js/customEvents.js
+++ js/customEvents.js
@@ -1,19 +1,5 @@
-jQuery(function($) {
-	// panelSnap plugin initiation
-    $('body').panelSnap({
-    	$menu: $('.menu'),
-    	slideSpeed: 500,
-    	keyboardNavigation: {
-    		enable:true
-    	}
-    });
-
-    //vertical center
-    $('.index-name').css({
-        'top': '50%',
-        'margin-top': -$('.index-name').height()/2
-    });
-
+$(document).ready(function(){
+  	$.adaptiveBackground.run();
 });
 
 
@@ -21,7 +7,7 @@ jQuery(function($) {
 $(window).scroll(function() {
 // fade in point of new nav at second page
 
-	if ($(window).scrollTop() > $(window).height()/1.2 ){
+	/*if ($(window).scrollTop() > $(window).height()/1.2 ){
 		//vertical nav
  		$('.menu').stop().animate({
    			opacity : 0.8
@@ -42,5 +28,5 @@ $(window).scroll(function() {
    		$('.menu-h').stop().animate({
    			opacity : 1
    		}, 50 );	
- 	};   	
+ 	};   */	
 });
